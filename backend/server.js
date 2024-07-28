b10c1c1386b002c1c40dadce6eae43451974e32f
Justin <justin.d.munoz@vanderbilt.edu>
2024-07-28 02:40:39 -0400

    Added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000; // Choose any port you prefer
 
 app.use(express.json());
+app.use(cors());
+
+const msg = "Node.js"
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js server!');
+    res.send(`Hello from ${msg} server!`);
 });
 
 app.listen(port, () => {
